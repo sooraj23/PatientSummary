6520304a286f3e3f6e28d837d1e30532dfd6de0b
hp <hp@SOOJU>
2025-10-27 21:32:02 +0000

    PatientSummary - Updated GPPractice names

--- PatientSummaryApi/Data/PatientRepository.cs
+++ PatientSummaryApi/Data/PatientRepository.cs
@@ -7,10 +7,10 @@ namespace PatientSummaryApi.Data
     {
         private readonly List<Patient> patients = new() { 
             new Patient { Id = 1, NHSNumber = "AS5352343TY25", Name = "Adam Smith", DateOfBirth = new DateTime(1999, 5, 12), GPPractice = "Northumbria Healthcare clinic" },
-            new Patient { Id = 2, NHSNumber = "JC6547892CT25", Name = "John Cockram", DateOfBirth = new DateTime(1989, 9, 7), GPPractice = "Northumbria Healthcare clinic" },
+            new Patient { Id = 2, NHSNumber = "JC6547892CT25", Name = "John Cockram", DateOfBirth = new DateTime(1989, 9, 7), GPPractice = "The City Road Medical Centre" },
             new Patient { Id = 3, NHSNumber = "SJ4561289PH25", Name = "Simon Jenkins", DateOfBirth = new DateTime(1970, 4, 25), GPPractice = "Northumbria Healthcare clinic" },
-            new Patient { Id = 4, NHSNumber = "LB7982564LD25", Name = "Liam Bibby", DateOfBirth = new DateTime(1985, 7, 16), GPPractice = "Northumbria Healthcare clinic" },
-            new Patient { Id = 5, NHSNumber = "AC7456321OT25", Name = "Alastair Campell", DateOfBirth = new DateTime(1979, 4, 1), GPPractice = "Northumbria Healthcare clinic" }
+            new Patient { Id = 4, NHSNumber = "LB7982564LD25", Name = "Liam Bibby", DateOfBirth = new DateTime(1985, 7, 16), GPPractice = "St Mary’s Surgery" },
+            new Patient { Id = 5, NHSNumber = "AC7456321OT25", Name = "Alastair Campell", DateOfBirth = new DateTime(1979, 4, 1), GPPractice = "The Maples Medical Centre" }
         };
         public Patient? GetPatientById(int Id)
         {
